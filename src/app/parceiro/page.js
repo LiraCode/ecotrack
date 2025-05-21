5e3d2f93bb0455d439c4558bfa2d71beebdab6d4
liracode <fls@ic.ufal.br>
2025-05-20 23:52:14 -0300

    add page.js of  parceiro route

---
+++ src/app/parceiro/page.js
@@ -0,0 +1,22 @@
+'use client';
+
+import AppLayout from '@/components/Layout/page';
+import { Box } from '@mui/material';
+
+export default function Administracao({}) {
+
+return (
+  <AppLayout>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Bem-vindo ao painel do parceiro!</h1>
+    </Box>
+  </AppLayout>
+);
+}
