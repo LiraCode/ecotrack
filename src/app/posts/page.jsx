a19566060eceb95e520ab42f8c96a0887a80d4cd
liracode <fls@ic.ufal.br>
2025-05-23 16:29:38 -0300

    post width

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -10,27 +10,31 @@ export default function PostsPage() {
 
   return (
     <AppLayout>
-        <div className="xs:ml-[100px] lg:mt-0 mt-4  flex  justify-center items-center h-95vh">
-      <div className={styles.contentContainer}>
-        {/* Abas de navegação */}
-        <div className={styles.tabsContainer}>
-          <button 
-            className={`${styles.tabButton} ${activeTab === 'posts' ? styles.activeTab : ''}`}
-            onClick={() => setActiveTab('posts')}
-          >
-            Posts
-          </button>
-          <button 
-            className={`${styles.tabButton} ${activeTab === 'trilha' ? styles.activeTab : ''}`}
-            onClick={() => setActiveTab('trilha')}
-          >
-            Trilha Ambiental
-          </button>
-        </div>
+      <div className="xs:ml-[100px] lg:mt-0 mt-4  flex  justify-center items-center  w-90vw h-95vh">
+        <div className={styles.contentContainer}>
+          {/* Abas de navegação */}
+          <div className={styles.tabsContainer}>
+            <button
+              className={`${styles.tabButton} ${
+                activeTab === "posts" ? styles.activeTab : ""
+              }`}
+              onClick={() => setActiveTab("posts")}
+            >
+              Posts
+            </button>
+            <button
+              className={`${styles.tabButton} ${
+                activeTab === "trilha" ? styles.activeTab : ""
+              }`}
+              onClick={() => setActiveTab("trilha")}
+            >
+              Trilha Ambiental
+            </button>
+          </div>
 
-        {activeTab === 'posts' ? <PostsContent /> : <TrilhaContent />}
-      </div>
+          {activeTab === "posts" ? <PostsContent /> : <TrilhaContent />}
         </div>
+      </div>
     </AppLayout>
   );
 }
