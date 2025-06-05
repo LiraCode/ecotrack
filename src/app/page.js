0524940cf615e6b8112617d7aeeae5361f82f9a9
liracode <fls@ic.ufal.br>
2025-06-04 22:38:19 -0300

    fix index img

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import AppLayout from "@/components/Layout/page";
 export default function Home() {
   return (
     <AppLayout>
-      <div className="px-4 sm:px-0 lg:ml-[100px] lg:mr-[0px] lg:mt-0 mt-4 w-full sm:w-92vh h-95vh justify-center items-center">
+      <div className="px-4 sm:px-0 lg:ml-[100px] lg:mr-[0px] lg:mt-0 mt-4 w-90vw  h-95vh justify-center items-center">
         <HomePage />
       </div>
     </AppLayout>
